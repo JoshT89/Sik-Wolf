3414026218c9afaa8b964558f6ca27fe74bcefad
JoshT89 <josh.h.t@live.co.uk>
2023-10-27 17:59:49 +0100

    Added Sik Wolf pics

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
     color: '#EFBD48',
     isLogoTexture: true,
     isFullTexture: false,
-    logoDecal: './threejs.png',
-    fullDecal: './threejs.png',
+    logoDecal: './sikwolf.png',
+    fullDecal: './sikwolf2.png',
 });
 
 export default state;
